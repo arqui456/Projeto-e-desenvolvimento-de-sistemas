198a715808af1600e757c1828c1a02b91d5ec500
eireneof <eof@ic.ufal.br>
2022-10-05 23:31:15 -0300

    refactor: arquitetura de pastas

--- Application/frontend/ru-admin-frontend/src/app/app.component.ts
+++ Application/frontend/ru-admin-frontend/src/app/app.component.ts
@@ -8,3 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ru-admin';
 }
+
+/* 
+
+- Padronziar as fontes e tags voltadas para texto
+- componente de input
+- componente de botão suprindo as todas as "aparências" (com opção de ícones)
+- ver qual biblioteca de ícones utilizar ou utilizar do fingma mesmo (https://fonts.google.com/icons?icon.query=close)
+- componente de navbar
+
+*/
