f393de0ece35f4fd5de2c48ea097304b6b89ac70
eireneof <eof@ic.ufal.br>
2022-10-18 00:34:12 -0300

    feat: primeiros componentes de layout e troca de sass para scss

--- Application/frontend/ru-admin-frontend/src/app/layout/layout.module.ts
+++ Application/frontend/ru-admin-frontend/src/app/layout/layout.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputComponent } from './input/input.component';
+import { ButtonComponent } from './button/button.component';
+import { IconComponent } from './icon/icon.component';
+import { LogoComponent } from './logo/logo.component';
+import {MatIconModule} from '@angular/material/icon';
+import { ContainerConteudoComponent } from './container-conteudo/container-conteudo.component'
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InputComponent,
+    ButtonComponent,
+    IconComponent,
+    LogoComponent,
+    ContainerConteudoComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
+  ],
+  exports: [
+    InputComponent,
+    ButtonComponent,
+    IconComponent,
+    LogoComponent,
+    ContainerConteudoComponent
   ]
 })
 export class LayoutModule { }
