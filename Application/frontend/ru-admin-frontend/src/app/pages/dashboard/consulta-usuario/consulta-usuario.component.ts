0c547250354d11af1123b5c1a6da89437e74a86d
eireneof <eof@ic.ufal.br>
2022-10-19 17:28:19 -0300

    feat: layout validação refeição

---
+++ Application/frontend/ru-admin-frontend/src/app/pages/dashboard/consulta-usuario/consulta-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-usuario',
+  templateUrl: './consulta-usuario.component.html',
+  styleUrls: ['./consulta-usuario.component.scss']
+})
+export class ConsultaUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
